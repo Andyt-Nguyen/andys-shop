00febe3d763c0fb19cef3a1408a23ac146c02597
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2017-10-14 23:48:28 -0700

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Main from './Components/Main';
-
+import NavBar from './Components/NavBar';
+import './index.css';
 const App = () => (
 	<div>
+	  <NavBar />
 		<Main />
 	</div>
 );
---
+++ src/Components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NavBar = () => (
+	<nav>
+		<div className="nav">
+			<i style={{fontSize:"20px"}} className="fa fa-bullseye"></i>
+			<a>Mac</a>
+			<a>iPad</a>
+			<a>iPhone</a>
+			<a>Watch</a>
+			<a>TV</a>
+			<a>Music</a>
+			<a>Support</a>
+		</div>
+	</nav>
+);
+
+export default NavBar;
